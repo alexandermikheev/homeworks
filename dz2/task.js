3780c76810e98a72a40cdaac45fcb32897b07d96
Александр Михеев <nyafkin2012@yandex.ru>
2022-10-04 08:21:26 +0300

    второе дз полностью

--- dz2/task.js
+++ dz2/task.js
@@ -6,15 +6,15 @@ function getArrayParams(arr) {
   sum = 0;
   avg = 0;
 
-  for (i = 0; i < arr.length; i++){
-    if(arr[i] < min){
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
       min = arr[i];
     }
-    if(arr[i] > max){
+    if (arr[i] > max) {
       max = arr[i];
     }
     sum += arr[i];
- 
+
   }
   avg = parseFloat((sum / arr.length).toFixed(2));
 
@@ -23,34 +23,25 @@ function getArrayParams(arr) {
   return { min: min, max: max, avg: avg };
 };
 
+
 // Задание 2
 function worker(arr) {
-  let sum;
-  
-
-
-  // Ваш код
-  function worker(arr){
-    for (i = 0; i < arr.length; i++){
-      sum =+ arr[i];
-    };
-    return 
-  }
-console.log(sum);
-
+  let sum = 0;
+  arr.forEach(item => sum += item);
   return sum;
 }
 
-function makeWork(arrOfArr, func) {
-  let max;
+function makeWork(arrOfArr, worker) {
+  let max = -Infinity;
 
-  // Ваш кода
-  // for ...
-  
+  for (let item of arrOfArr) {
+    let result = worker(item);
+    max = max > result ? max : result;
+  }
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  return getArrayParams(arr).max - getArrayParams(arr).min
 }
