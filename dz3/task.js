f1d5c80cf391c79e3ff319c93691e75dc01ad0ac
Alexander Mikheev <nyafkin2012@yandex.ru>
2022-10-12 18:23:32 +0300

    dz3

---
+++ dz3/task.js
@@ -0,0 +1,4 @@
+function compareArrays(arr1, arr2) { return  arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index]); };
+
+function advancedFilter (arr){ return arr.filter(item => item > 0 && item % 3 === 0).map(item => item*10)};
+  
